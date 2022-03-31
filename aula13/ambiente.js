409d1b59909670089266e4f3e21979c1b08852f1
Aluilson Peixoto <98779797+Peixoto1990@users.noreply.github.com>
2022-03-31 09:46:59 -0300

    Criei aula 13
    criei o ex ambiente, para estruturas de repetições.

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+let cont = 1
+
+do {
+    console.log(`passo ${cont}`)
+    cont ++
+} while (cont <= 6) //repetição com teste lógico no final
+
+/*
+let cont = 1
+
+while (cont <= 6) {
+    console.log(`passo ${cont}`)
+    cont ++
+}
+
+repetição com teste lógico no início
+*/
+
+/*
+console.log('passo 1')
+console.log('passo 2')
+console.log('passo 3')
+console.log('passo 4')
+console.log('passo 5')
+console.log('passo 6')
+
+sem estrutura de repetição
+*/
