3811de4c0767b2415ab8d1c74aeede5e6a6eb6cf
Aluilson Peixoto <98779797+Peixoto1990@users.noreply.github.com>
2022-04-04 09:27:43 -0300

    Criei aula 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,16 @@
+let cont = 1
+
+/*
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont ++
+}
+*/
+
+console.log('Vai começar...')
+
+for (cont = 1; cont <= 6; cont ++) { //Estrutura de repetição, com variável de controle.
+    console.log(`Passo ${cont}`)
+}
+
+console.log('Fim!')
