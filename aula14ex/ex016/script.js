913eee9ffae43926c553ba7a19035809e324828b
Aluilson Peixoto <98779797+Peixoto1990@users.noreply.github.com>
2022-04-08 10:42:03 -0300

    fiz o ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,30 @@
-window.alert('Olá!')
+function calcular() {
+    let inicio
+    let fim
+    let pulo
+    let cont
+
+    if (window.document.getElementById('inicio').value.length == 0 || window.document.getElementById('pulo').value.length == 0 || window.document.getElementById('fim').value.length == 0) {
+        window.document.getElementById('resultado').innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    } else {
+        inicio = Number(window.document.getElementById('inicio').value)
+        fim = Number(window.document.getElementById('fim').value)
+        pulo = Math.abs(Number(window.document.getElementById('pulo').value))
+        if (pulo == 0) {
+            pulo = 1
+            window.alert('Impossível contar com PASSO = 0. Considerando PASSO = 1.')
+        } 
+        window.document.getElementById('resultado').innerHTML = 'Contando... <br>'
+        if (inicio < fim) { //contagem crescente
+            for (cont = inicio; cont <= fim; cont += pulo) {
+                window.document.getElementById('resultado').innerHTML += `${cont} 👉 `
+            }
+        } else if (inicio > fim) { //contagem regressiva
+            for (cont = inicio; cont >= fim; cont -= pulo) {
+                window.document.getElementById('resultado').innerHTML += `${cont} 👉 `
+            }
+        }
+        window.document.getElementById('resultado').innerHTML += `🏁`
+    } 
+}
