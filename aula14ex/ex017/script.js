54bb7e4fd61689606174299f15a3f422d84863fe
Aluilson Peixoto <98779797+Peixoto1990@users.noreply.github.com>
2022-04-10 09:37:38 -0300

    Fiz o ex017
    Modifiquei ex016

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-window.alert('Olá!')
+function calcular() {
+    if (window.document.getElementById('num').value.length == 0) {
+        window.alert('Dados inválidos. Por favor, digite um número.')
+    } else {
+        let num = Number(window.document.getElementById('num').value)
+        let cont
+        window.document.getElementById('seltab').innerHTML = ''
+        for (cont = 1; cont <= 10; cont ++) {
+            let item = document.createElement('option')
+            item.innerText = `${num} * ${cont} = ${num * cont}`
+            item.value = `tab${cont}`
+            window.document.getElementById('seltab').appendChild(item)
+        }
+        
+    }
+
+}
